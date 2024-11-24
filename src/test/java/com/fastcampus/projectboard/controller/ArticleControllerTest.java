0a56cf3a70b7f320f95b0569da8ce5a1badf5713
Hwangseoeun <sunny030921@gmail.com>
2024-11-24 18:06:20 +0900

    #17 - 게시판 뷰 기본 골격 작성
     controller, view, test까지 한 번에 적용

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -37,7 +37,7 @@ class ArticleControllerTest {
 
     }
 
-    @Disabled("구현중")
+//    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
